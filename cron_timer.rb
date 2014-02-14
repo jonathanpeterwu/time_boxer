c8b19fa1163d391713e01ba4d1b51a41153ef78f
Mathilde Mouw & Stephen Buchanan <pair+mathildemouw.papapoison@devbootcamp.com>
2014-02-14 14:28:00 -0800

    cron_timer can open and edit our temporary crontab file

--- cron_timer.rb
+++ cron_timer.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 module CronJobs
 
   def cron_converter(time) #returns string of now in cronspk
@@ -7,7 +9,21 @@ module CronJobs
   def cron_timer(minutes_from_now, script)
     raw_time = Time.now + (minutes_from_now*60)
     cron_string = cron_converter(raw_time)
-    jo = File.open #still need to write crontab protocol
+    job = File.open('temp_crontab.txt')
+    job << "#{cron_string} ruby #{script}"
+    #call cron_timer to add cronjob 1 minute later that clears cronjob list
+
+    #finally, this method needs to connect our contab to running cron
+  ensure
+    job.close
+
+  end
+
+  def cron_stop(minutes_from_now_plus)
+    end_job = File.open('temp_crontab.txt')
+    end_job << "#{minutes_from_now_plus} crontab -r" #will this work?
+  ensure
+    end_job.close
   end
 
 end
